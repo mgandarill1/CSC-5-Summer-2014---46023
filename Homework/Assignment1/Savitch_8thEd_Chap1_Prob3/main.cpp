3eab9ec81bc280e6e4ccf3ce36d9f43013c95051
mgandarill1 <mgandarill1@student.rcc.edu>
2014-06-25 13:19:53 -0700

    Signed-off-by: mgandarill1 <mgandarill1@student.rcc.edu>

---
+++ Homework/Assignment1/Savitch_8thEd_Chap1_Prob3/main.cpp
@@ -0,0 +1,44 @@
+/* 
+ * File:   main.cpp
+ * Author: Mayra Gandarilla
+ * Purpose: Homework Project
+ * Created on June 24, 2014, 5:51 PM
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Defined Libraries
+
+//Global Conversions
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main( ) 
+{
+    
+    int numpods=10, peaspod=0, ttalps;
+    cout << "Hello ";
+    cout << "Press return after entering a number.\n";
+    cout << "Enter the number of pods:\n";
+    cout << numpods << endl;
+    cout << "Enter the number of peas in a pod:\n";
+    cout << peaspod << endl;
+    ttalps = numpods / peaspod;
+    cout << "If you have ";
+    cout << numpods;
+    cout << " pea pods\n";
+    cout << "and ";
+    cout << peaspod;
+    cout << " peas in each pod, then\n";
+    cout << "you have ";
+    cout << ttalps;
+    cout << " peas in all the pods.\n";
+    cout << "Good-bye\n";
+    
+
+    return 0;
+}
+
