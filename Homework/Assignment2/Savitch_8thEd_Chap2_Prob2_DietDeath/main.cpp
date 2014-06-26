ea03b2c09a98394fe6a0dd03dee87e515ba2ce47
mgandarill1 <mgandarill1@student.rcc.edu>
2014-06-26 13:54:35 -0700

    Signed-off-by: mgandarill1 <mgandarill1@student.rcc.edu>

---
+++ Homework/Assignment2/Savitch_8thEd_Chap2_Prob2_DietDeath/main.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.cpp
+ * Author: Mayra Gandarilla
+ * Created on June 26, 2014, 1:50 PM
+ */
+
+// System Level Libraries
+#include <iostream>
+using namespace std;
+
+// User Defined Libraries
+
+// Global Conversions
+
+// Function Prototypes
+
+// Execution Begins Here!
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
