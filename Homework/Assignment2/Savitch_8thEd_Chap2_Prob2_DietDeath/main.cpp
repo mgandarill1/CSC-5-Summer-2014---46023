a1d8329d46eb742be2cb65fab196479292a22c99
mgandarill1 <mgandarill1@student.rcc.edu>
2014-06-30 13:56:35 -0700

    Signed-off-by: mgandarill1 <mgandarill1@student.rcc.edu>

--- Homework/Assignment2/Savitch_8thEd_Chap2_Prob2_DietDeath/main.cpp
+++ Homework/Assignment2/Savitch_8thEd_Chap2_Prob2_DietDeath/main.cpp
@@ -11,12 +11,29 @@ using namespace std;
 // User Defined Libraries
 
 // Global Conversions
+const float ATSWT = .001;
 
 // Function Prototypes
 
 // Execution Begins Here!
 int main(int argc, char** argv) {
-
+   float 
+        msdeth, // Mouse Death
+        mswt,   // Mouse Weight
+        uswt,   // User Weiht
+        usdeth; // User Death
+    
+    cout<<"Please enter the amount in ounces of artificial sweetener needed to kill a mouse.\n";
+    cin>>msdeth;
+    cout<<"Please enter the weight of the mouse in lbs.\n";
+    cin>>mswt;
+    cout<<"Please enter the user's desired weight in lbs.\n";
+    cin>>uswt;
+    msdeth = mswt * ATSWT, usdeth = uswt * ATSWT;
+    cout<<msdeth<<" is required to kill a mouse and "<<usdeth<<" is required to kill user."<<endl;
+            
+    
+            
     return 0;
 }
 
