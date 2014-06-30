869b18f4b45ace1402eeac8c4901bdeadbac9fb8
mgandarill1 <mgandarill1@student.rcc.edu>
2014-06-30 13:58:55 -0700

    Signed-off-by: mgandarill1 <mgandarill1@student.rcc.edu>

--- Homework/Assignment2/Savitch_8thEd_Chap2_Prob2_DietDeath/main.cpp
+++ Homework/Assignment2/Savitch_8thEd_Chap2_Prob2_DietDeath/main.cpp
@@ -22,7 +22,7 @@ int main(int argc, char** argv) {
         mswt,   // Mouse Weight
         uswt,   // User Weiht
         usdeth; // User Death
-    
+   do {
     cout<<"Please enter the amount in ounces of artificial sweetener needed to kill a mouse.\n";
     cin>>msdeth;
     cout<<"Please enter the weight of the mouse in lbs.\n";
@@ -31,7 +31,7 @@ int main(int argc, char** argv) {
     cin>>uswt;
     msdeth = mswt * ATSWT, usdeth = uswt * ATSWT;
     cout<<msdeth<<" is required to kill a mouse and "<<usdeth<<" is required to kill user."<<endl;
-            
+   } while (msdeth=!0);       
     
             
     return 0;
