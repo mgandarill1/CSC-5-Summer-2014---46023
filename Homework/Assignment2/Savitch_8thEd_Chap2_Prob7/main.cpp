82134cecce99b455d5058f8184afc562e2dd21e5
mgandarill1 <mgandarill1@student.rcc.edu>
2014-07-01 13:35:53 -0700

    Signed-off-by: mgandarill1 <mgandarill1@student.rcc.edu>

---
+++ Homework/Assignment2/Savitch_8thEd_Chap2_Prob7/main.cpp
@@ -0,0 +1,25 @@
+/* 
+ * File:   main.cpp
+ * Author: Mayra Gandarilla
+ * Purpose: Assignment 2
+ * Created on July 1, 2014, 11:54 AM
+ */
+
+// System Level Libraries
+#include <iostream>
+using namespace std;
+
+// User Defined Libraries
+
+// Global Constants
+
+//Function Prototypes
+
+// Execution Begins Here!
+int main(int argc, char** argv) {
+    //Define Variables
+    
+
+    return 0;
+}
+
