0404c795a5c68d17c811cdcc9b8618166a70ef35
mgandarill1 <mgandarill1@student.rcc.edu>
2014-07-02 11:11:29 -0700

    Signed-off-by: mgandarill1 <mgandarill1@student.rcc.edu>

--- Homework/Assignment2/Savitch_8thEd_Chap2_Prob13/main.cpp
+++ Homework/Assignment2/Savitch_8thEd_Chap2_Prob13/main.cpp
@@ -6,13 +6,17 @@
  */
 
 // System Level Libraries
+#include <cstdlib>
 #include <iostream>
+#include <ctime>
 using namespace std;
 
 
 // User Defined Libraries
 
 // Global Constants
+const float CNVhr2m = 60.0f;    // Hours to minutes
+const float CNVm2sc = CNVhr2m;  // Minutes to seconds
 
 // Function Prototypes
 
@@ -20,6 +24,23 @@ using namespace std;
 int main(int argc, char** argv) {
     
    //Define Variables
+    float   mph,        // Miles Per Hour
+            fmin;       // Float Minutes
+    int     inMin,      // Integer Minute
+            inSec;      // Integer Seconds
+    
+    // Random number seed
+    srand(static_cast<unsigned int>(time(0)));
+    
+    // Calculations
+    mph=rand()%28+1;            // Speed of fastest human, range from 1 - 28
+    fmin=CNVhr2m/mph;           // Floating point minutes to miles
+    inMin=fmin;                 
+    inSec=(fmin-inMin)*CNVm2sc;
+    
+    // Output Results
+    cout<<"Speed is "<<mph<<"mph.\n";
+    cout<<"Equal to 1 mile in "<<inMin<<" minutes and "<<inSec<<" seconds."<<endl;
     
     
 
