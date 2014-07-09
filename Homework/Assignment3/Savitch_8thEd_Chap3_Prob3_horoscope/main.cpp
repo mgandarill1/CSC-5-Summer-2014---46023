6eeb741ef0c536fe460e2e023848053647e9ef69
mgandarill1 <mgandarill1@student.rcc.edu>
2014-07-09 13:41:06 -0700

    Signed-off-by: mgandarill1 <mgandarill1@student.rcc.edu>

---
+++ Homework/Assignment3/Savitch_8thEd_Chap3_Prob3_horoscope/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on July 9, 2014, 1:40 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
